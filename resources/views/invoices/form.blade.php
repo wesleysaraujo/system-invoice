f2214c2138f0cea6e3fcfb1e14688979d2dd2cb6
Wesley S. Araújo <wesley.araujo@e-create.com.br>
2016-10-13 18:45:53 -0300

    Ajuste no formulario Create

--- resources/views/invoices/form.blade.php
+++ resources/views/invoices/form.blade.php
@@ -11,7 +11,35 @@
 			<p v-if="errors.client" class="error">@{{ errors.client[0] }}</p>
 		</div>
 	</div>
-	<div class="col-sm-4">
-		
+	<div class="col-sm-12">
+		<div class="form-group">
+			<label for="client-address">Endereço do Cliente</label>
+			<textarea name="client-address" id="client-address" class="form-control" v-model="form.client_address"></textarea>
+			<p v-if="errors.client_address" class="error">@{{errors.client_address[0]}}</p>
+		</div>
 	</div>
+	<div class="col-sm-6">
+        <div class="form-group">
+            <label>Título</label>
+            <input type="text" class="form-control" v-model="form.title">
+            <p v-if="errors.title" class="error">@{{errors.title[0]}}</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <label>Data da Fatura</label>
+                <input type="date" class="form-control" v-model="form.invoice_date">
+                <p v-if="errors.invoice_date" class="error">@{{errors.invoice_date[0]}}</p>
+            </div>
+            <div class="col-sm-6">
+                <label>Data de Vencimento</label>
+                <input type="date" class="form-control" v-model="form.due_date">
+                <p v-if="errors.due_date" class="error">@{{errors.due_date[0]}}</p>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<div v-if="errors.products_empty">
+	<p class="alert alert-danger">@{{errors_products_empty[0]}}</p>
+	<hr>
 </div>
